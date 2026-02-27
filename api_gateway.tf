150d453956c52b8fff63f09b1b35ac2dbab27291
JTarazona00 <jtarazonaf2@upao.edu.pe>
2026-02-26 21:04:59 -0500

    wip: -"aws_apigatewayv2_stage" "default": Stage es el ambiente de publicacio del API - "aws_apigatewayv2_integration" "alb_integration": Define hacia dónde reenvía el API Gateway las peticiones que recibe. Es el puente entre el API Gateway y el ALB. - "aws_apigatewayv2_route" "proxy_route": La regla de enrutamiento. Le dice al API Gateway qué hacer con las peticiones que llegan.

--- api_gateway.tf
+++ api_gateway.tf
@@ -18,3 +18,43 @@ resource "aws_apigatewayv2_api" "http_api" {
     Environment = var.environment
   }
 }
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gateway_logs.arn
+    format = jsonencode({
+      requestId      = "$context.requestId"
+      ip             = "$context.identity.sourceIp"
+      requestTime    = "$context.requestTime"
+      httpMethod     = "$context.httpMethod"
+      routeKey       = "$context.routeKey"
+      status         = "$context.status"
+      protocol       = "$context.protocol"
+      responseLength = "$context.responseLength"
+    })
+  }
+
+  tags = {
+    Name        = "${var.project_name}-api-stage"
+    Environment = var.environment
+  }
+}
+
+resource "aws_apigatewayv2_integration" "alb_integration" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  integration_type   = "HTTP_PROXY"
+  integration_method = "ANY"
+  integration_uri    = "http://${aws_lb.main_alb.dns_name}"
+  connection_type    = "INTERNET"
+}
+
+resource "aws_apigatewayv2_route" "proxy_route" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  route_key          = "ANY /{proxy+}"
+  target             = "integrations/${aws_apigatewayv2_integration.alb_integration.id}"
+  authorization_type = "AWS_IAM"
+}
