ee8d5edd2aa5304d0a1d7c2f292e61ace944204f
unknown <bratwelsniper_2051@hotmail.com>
2026-02-26 21:34:37 -0500

    feat: Configuracion AIM para AWS users

---
+++ iam_groups.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_group" "admins" {
+  name = "${var.project_name}-admins"
+}
+
+resource "aws_iam_group_policy_attachment" "admin_attach" {
+  group      = aws_iam_group.admins.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_group" "developers" {
+  name = "${var.project_name}-developers"
+}
+
+resource "aws_iam_group_policy_attachment" "developer_attach" {
+  group      = aws_iam_group.developers.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
