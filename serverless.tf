bbc935aa72b233b6ac850a1a781e3d3ae8637e43
JTarazona00 <jtarazonaf2@upao.edu.pe>
2026-02-26 22:51:37 -0500

    wip: aplicacion de los resource 2 - aws_iam_role_policy lambda_sqs: Le da permiso a Lambda para escribir mensajes en las dos colas SQS del proyecto. - aws_signer_signing_profile lambda_signing: Perfil de firma de código con validez de 5 años, garantiza que el código no fue alterado. - aws_lambda_code_signing_config lambda_code_signing: Aplica la firma al Lambda, avisa si el código desplegado no está firmado. - aws_lambda_function error_func: La función Lambda principal, recibe alertas de SNS, las procesa y envía emails via SES. - aws_sns_topic_subscription lambda_sub: Suscribe Lambda al canal SNS de alertas para que se ejecute automáticamente cuando hay una alarma. - aws_lambda_permission with_sns: Le da permiso explícito a SNS para poder invocar la función Lambda.

--- serverless.tf
+++ serverless.tf
@@ -82,3 +82,103 @@ resource "aws_iam_role_policy" "lambda_ses" {
     }]
   })
 }
+
+resource "aws_iam_role_policy" "lambda_sqs" {
+  name = "${var.project_name}-lambda-sqs"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "sqs:SendMessage",
+        "sqs:GetQueueAttributes",
+        "sqs:GetQueueUrl"
+      ]
+      Resource = [
+        aws_sqs_queue.lambda_dlq.arn,
+        aws_sqs_queue.error_queue.arn
+      ]
+    }]
+  })
+}
+
+resource "aws_signer_signing_profile" "lambda_signing" {
+  platform_id = "AWSLambda-SHA384-ECDSA"
+  name        = "${replace(var.project_name, "-", "")}lambdasigning${var.environment}"
+
+  signature_validity_period {
+    value = 5
+    type  = "YEARS"
+  }
+
+  tags = {
+    Name        = "${var.project_name}-lambda-signing"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lambda_code_signing_config" "lambda_code_signing" {
+  description = "Code signing config for Lambda functions"
+
+  allowed_publishers {
+    signing_profile_version_arns = [aws_signer_signing_profile.lambda_signing.arn]
+  }
+
+  policies {
+    untrusted_artifact_on_deployment = "Warn"
+  }
+}
+
+resource "aws_lambda_function" "error_func" {
+  filename                       = "lambda/funcion_error.zip"
+  function_name                  = "${var.project_name}-error-handler"
+  role                           = aws_iam_role.lambda_role.arn
+  handler                        = "index.handler"
+  runtime                        = "nodejs20.x"
+  source_code_hash               = filebase64sha256("lambda/funcion_error.zip")
+  reserved_concurrent_executions = 50
+  code_signing_config_arn        = aws_lambda_code_signing_config.lambda_code_signing.arn
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
+  }
+
+  dead_letter_config {
+    target_arn = aws_sqs_queue.lambda_dlq.arn
+  }
+
+  tracing_config {
+    mode = "Active"
+  }
+
+  environment {
+    variables = {
+      ALERT_EMAIL = var.alert_email
+      ENVIRONMENT = var.environment
+    }
+  }
+
+  kms_key_arn = aws_kms_key.main.arn
+
+  tags = {
+    Name        = "${var.project_name}-error-handler"
+    Environment = var.environment
+  }
+}
+
+resource "aws_sns_topic_subscription" "lambda_sub" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.error_func.arn
+}
+
+resource "aws_lambda_permission" "with_sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.error_func.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.alerts.arn
+}
